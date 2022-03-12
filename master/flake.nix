13ab65649f2e0e5b75693d5c57146007b926fcca
hugosenari <hugosenari@gmail.com>
2022-03-12 04:14:58 -0300

    chore: re index nimcef

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Nim wrapper for the Chromium Embedded Framework'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimCEF-master.flake = false;
+  inputs.src-nimCEF-master.owner = "jangko";
+  inputs.src-nimCEF-master.ref   = "refs/heads/master";
+  inputs.src-nimCEF-master.repo  = "nimCEF";
+  inputs.src-nimCEF-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimCEF-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimCEF-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
